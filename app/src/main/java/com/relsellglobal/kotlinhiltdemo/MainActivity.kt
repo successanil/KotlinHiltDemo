5d2f2290a5349da9b22d0be7b7611919dfe86232
Relsell <relsellglobal@gmail.com>
2022-06-25 12:10:44 +0530

    adding sample list in compose

--- app/src/main/java/com/relsellglobal/kotlinhiltdemo/MainActivity.kt
+++ app/src/main/java/com/relsellglobal/kotlinhiltdemo/MainActivity.kt
@@ -3,15 +3,19 @@ package com.relsellglobal.kotlinhiltdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.relsellglobal.flowdemoapplication.ui.theme.KotlinWeatherDataUI2Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +25,8 @@ class MainActivity : ComponentActivity() {
             KotlinWeatherDataUI2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    initialView()
+                    //initialView()
+                    loadList()
                 }
             }
         }
@@ -51,5 +56,27 @@ fun initialView() {
             Greeting("Anil")
             Greeting("Sunil")
         }
+    }
+}
+
+@Composable
+fun loadList() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        for(i in 1..50) {
+            Text(text="Item $i",
+                fontSize=24.sp,
+                fontWeight= FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+
+
+            )
+        }
+
     }
 }
